73af3ecc7549343663acfc699f1df9edea6ac187
Mr. Mike <mjtabbert@gmail.com>
2026-03-31 22:25:51 -0500

    Update project configuration and push notification target files

---
+++ Push/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  Push
+//
+//  Created by Mike Tabbert on 3/31/26.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
